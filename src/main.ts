4aad1c8b49efd4eeebbff361a8d85691a615fad7
leedawnn <2g_hee@naver.com>
2024-01-03 16:32:29 +0900

    feat: boards 모듈 생성

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule); // 가장 큰 모듈(root) : AppModule
+  await app.listen(3000); // 3000번 포트에서 실행한다.
 }
 bootstrap();

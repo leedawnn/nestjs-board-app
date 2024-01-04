0e34a7444dcc94b23b66caffdc887c85782525b3
leedawnn <2g_hee@naver.com>
2024-01-04 17:40:40 +0900

    feat: 게시물 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards') // 앤드포인트
 export class BoardsController {
@@ -7,7 +8,21 @@ export class BoardsController {
 
   // Handler
   @Get()
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
+
+  // 이렇게 사용해도 되네?
+  // @Post()
+  // createBoard(@Body() body) {
+  //   return this.boardsService.createBoard(body.title, body.description);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-  private boards = []; // 다른 컴포넌트에서 배열 값을 수정할 수 있기 때문에, private 사용!
+  private boards: Board[] = []; // 다른 컴포넌트에서 배열 값을 수정할 수 있기 때문에, private 사용!
 
-  getAllBoards() {
+  // return 값도 타입 지정!
+  getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, desription: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      desription,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board); // 생성 기능이니까 boards 안에 넣어줘야겠지?
+    return board; // board 리턴값
   }
 }
